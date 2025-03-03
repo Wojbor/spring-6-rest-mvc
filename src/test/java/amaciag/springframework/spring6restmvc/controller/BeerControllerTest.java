7910a5bd30d0ee3902198fe0b2a1f3bfd08bc6d6
Wojbor <arkadiusz.maciag1@gmail.com>
2025-03-03 21:08:42 +0100

    spring update + junits

--- src/test/java/amaciag/springframework/spring6restmvc/controller/BeerControllerTest.java
+++ src/test/java/amaciag/springframework/spring6restmvc/controller/BeerControllerTest.java
@@ -1,22 +1,57 @@
 package amaciag.springframework.spring6restmvc.controller;
 
+import amaciag.springframework.spring6restmvc.model.Beer;
+import amaciag.springframework.spring6restmvc.services.BeerService;
+import amaciag.springframework.spring6restmvc.services.BeerServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.UUID;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-import static org.junit.jupiter.api.Assertions.*;
 
-
-@SpringBootTest
+@WebMvcTest(BeerController.class)
 class BeerControllerTest {
 
     @Autowired
-    BeerController beerController;
+    MockMvc mockMvc;
+
+    @MockitoBean
+    BeerService beerService;
+
+    BeerServiceImpl beerServiceImpl = new BeerServiceImpl();
 
     @Test
-    void getBeerById() {
-        System.out.println(beerController.getBeerById(UUID.randomUUID()));
+    void listBeer() throws Exception {
+
+        given(beerService.listBeers()).willReturn(beerServiceImpl.listBeers());
+
+
+        mockMvc.perform(get("/api/v1/beer")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.length()", is(3)));
+
+    }
+
+    @Test
+    void getBeerById() throws Exception {
+        Beer testBeer = beerServiceImpl.listBeers().get(0);
+
+        given(beerService.getBeerById(testBeer.getId())).willReturn(testBeer);
+
+        mockMvc.perform(get("/api/v1/beer/" + testBeer.getId())
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id", is(testBeer.getId().toString())))
+                .andExpect(jsonPath("$.beerName", is(testBeer.getBeerName())));
     }
 }
--- src/test/java/amaciag/springframework/spring6restmvc/controller/CustomerControllerTest.java
+++ src/test/java/amaciag/springframework/spring6restmvc/controller/CustomerControllerTest.java
@@ -1,22 +1,51 @@
 package amaciag.springframework.spring6restmvc.controller;
 
+import amaciag.springframework.spring6restmvc.model.Customer;
+import amaciag.springframework.spring6restmvc.services.CustomerService;
+import amaciag.springframework.spring6restmvc.services.CustomerServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.UUID;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-@SpringBootTest
+@WebMvcTest(CustomerController.class)
 class CustomerControllerTest {
-    
-    @Autowired
-    CustomerController customerController;
 
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockitoBean
+    CustomerService customerService;
+
+    CustomerServiceImpl customerServiceImpl = new CustomerServiceImpl();
 
     @Test
-    void getCustomerById() {
-        System.out.println(customerController.getCustomerById(UUID.randomUUID()));
+    void lisCustomer() throws Exception {
+        given(customerService.listCustomers()).willReturn(customerServiceImpl.listCustomers());
+
+        mockMvc.perform(get("/api/v1/customer").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.length()", is(3)));
+    }
+
+    @Test
+    void getCustomerById() throws Exception {
+        Customer testCustomer = customerServiceImpl.listCustomers().get(0);
+
+        given(customerService.getCustomerById(testCustomer.getId())).willReturn(testCustomer);
+
+        mockMvc.perform(get("/api/v1/customer/" +testCustomer.getId()).accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id", is(testCustomer.getId().toString())))
+                .andExpect(jsonPath("$.customerName", is(testCustomer.getCustomerName())));
     }
 }

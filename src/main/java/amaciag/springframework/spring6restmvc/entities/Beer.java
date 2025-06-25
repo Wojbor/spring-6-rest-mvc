ba932300bee9c7646da2fa9c37c7ac8dad60a1cf
Wojbor <arkadiusz.maciag1@gmail.com>
2025-06-25 21:27:27 +0200

    simple jpa entities

---
+++ src/main/java/amaciag/springframework/spring6restmvc/entities/Beer.java
@@ -0,0 +1,33 @@
+package amaciag.springframework.spring6restmvc.entities;
+
+import amaciag.springframework.spring6restmvc.model.BeerStyle;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Version;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Beer {
+
+    @Id
+    private UUID id;
+    @Version
+    private Integer version;
+    private String beerName;
+    private BeerStyle beerStyle;
+    private String upc;
+    private Integer quantityOnHand;
+    private BigDecimal price;
+    private LocalDateTime createdDate;
+    private LocalDateTime updateDate;
+
+}
